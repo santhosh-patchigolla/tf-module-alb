8bdda7747ed84adb6049b6fa6698ff47806b26ba
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-08-07 14:06:38 +0530

    sdc

--- sg.tf
+++ sg.tf
@@ -1,5 +1,6 @@
 # Public load balancer Security group
 resource "aws_security_group" "alb_public" {
+  count       = var.INTERNAL ? 0 : 1
   name        = "roboshop-${var.ENV}-public-alb-sg"
   description = "Allows Only Traffic to internet"
   vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
@@ -26,6 +27,7 @@ resource "aws_security_group" "alb_public" {
 
 # Private load balancer security Group
 resource "aws_security_group" "alb_private" {
+  count       = var.INTERNAL ? 0 : 1  
   name        = "roboshop-${var.ENV}-private-alb-sg"
   description = "Allows Only Traffic to interanet"
   vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
